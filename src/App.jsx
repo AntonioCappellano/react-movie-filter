76c059027cc85fb8af2409d647b3b3a0cda574d8
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-12 13:40:58 +0100

    feat: implement initial film list and genre selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
-export default function App() {}
+import { useState } from "react";
+
+const initialFilm = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
+
+export default function App() {
+  // Setto valore iniziale
+  const [genre, setGenre] = useState("");
+
+  return (
+    <div>
+      <h1>Filtro</h1>
+      <div>
+        <label>Choose Genre</label>
+        <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+          <option value="">Tutti</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+      </div>
+      <ul></ul>
+    </div>
+  );
+}
